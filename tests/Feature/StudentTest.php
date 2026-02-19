221c58a538683241e6b4384157c0ae6d866dbbe3
Luca <larmendarizl23@fpsanturtzilh.eus>
2026-02-19 09:37:47 +0100

    studentTestExampleCompleted

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -1,34 +1,64 @@
 <?php
 
 use App\Models\Student;
+use App\Models\User;
+use App\Models\Group;
+use Laravel\Sanctum\Sanctum;
+
 
 test('show student', function () {
-    $student = Student::factory()->create();
+    $user = User::create([
+        'username' => 'Juan',
+        'email' => 'hola@hola.com',
+        'password' => 'hola',
+        'rol' => 'A'
+    ]);
+
+    Sanctum::actingAs($user);
+
+    $group = Group::create([
+        'name' => '3WAG2'
+    ]);
+
+    $student = Student::create([
+        'user_id' => $user->id,
+        'name' => 'Juan',
+        'surnames' => 'Perez',
+        'group_id' => $group->id,
+    ]);
 
     $response = $this->get("/api/students/{$student->id}");
-
     $response->assertStatus(200);
 });
 
 test('store student', function () {
     $payload = [
+        'first_name' => 'Laura',
+        'last_name' => 'Gomez',
+        'email' => 'laura@example.com',
+        'birth_date' => '2001-05-05'
+    ];
+
+    $response = $this->post("/api/students", $payload);
+    $response->assertStatus(201)
+             ->assertJsonFragment(['email' => 'laura@example.com']);
+});
+
+test('index students', function () {
+    Student::create([
         'first_name' => 'Juan',
         'last_name' => 'Perez',
         'email' => 'juan@example.com',
         'birth_date' => '2000-01-01'
-    ];
-
-    $response = $this->post("/api/students", $payload);
-
-    $response->assertStatus(201)
-             ->assertJsonFragment(['email' => 'juan@example.com']);
-});
-
-test('index students', function () {
-    Student::factory()->count(3)->create();
+    ]);
+    Student::create([
+        'first_name' => 'Laura',
+        'last_name' => 'Gomez',
+        'email' => 'laura@example.com',
+        'birth_date' => '2001-05-05'
+    ]);
 
     $response = $this->get("/api/students");
-
     $response->assertStatus(200)
-             ->assertJsonCount(3);
+             ->assertJsonCount(2);
 });
